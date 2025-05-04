b6f01ff4d16a0cabb9ff24b808d0b8cd2ddfe300
Sujeet Baisane <118635012+Fncbravo@users.noreply.github.com>
2025-05-04 13:57:55 +0530

    first commit todo

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("#### Welcome to our Todolist App! #####")	
+	fmt.Println("1 .Watch go crash course")
+	fmt.Println("2. Watch Nana's Golang Full course")
+	fmt.Println("3. Reward myself with a cheesecake")
+}
