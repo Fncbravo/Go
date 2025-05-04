d5d4c6d29eec6ef3209fe442519e78db125d997c
Sujeet Baisane <118635012+Fncbravo@users.noreply.github.com>
2025-05-04 23:46:40 +0530

    updated

--- main.go
+++ main.go
@@ -1,10 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("#### Welcome to our Todolist App! #####")	
-	fmt.Println("1 .Watch go crash course")
-	fmt.Println("2. Watch Nana's Golang Full course")
-	fmt.Println("3. Reward myself with a cheesecake")
+	fmt.Println("##### Welcome to our Todolist App! #####")
+	http.ListenAndServe(":8080", nil)
+
+	// var shortGolang = "Watch go crash course"
+	// var fullGolang = "Watch TWN golang Full course"
+	// var rewardDessert = "Reward myself with a donut"
+	// var taskItems = []string{shortGolang, fullGolang, rewardDessert}
+
+	// printTasks(taskItems)
+	// fmt.Println()
+
+	// taskItems = addTask(taskItems, "Go for a run")
+	// taskItems = addTask(taskItems, "Practice coding in go")
+
+	// fmt.Println("Updated list")
+	// printTasks(taskItems)
+
+}
+
+func printTasks(taskItems []string) {
+	fmt.Println("List of my Todos")
+	for index, task := range taskItems {
+		fmt.Printf("%d. %s\n", index+1, task)
+	}
+}
+
+func addTask(taskItem []string, newTask string) []string {
+
+	var updatedTaskItems = append(taskItem, newTask)
+	return updatedTaskItems
 }
